261db0b96e0194c9ff2586c6e6445b6fff9383f0
yallablagan <mafia.love.92@gmail.com>
2017-06-23 04:17:46 +0300

    no message

--- SMS/src/sysAdmin/AddCourseUI.java
+++ SMS/src/sysAdmin/AddCourseUI.java
@@ -8,14 +8,16 @@ import javax.swing.JTextField;
 import java.awt.Font;
 import javax.swing.JList;
 import javax.swing.AbstractListModel;
+import java.awt.Choice;
 
 public class AddCourseUI extends JPanel {
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+	private JTextField txtcourseid;
+	private JTextField txtcoursename;
+	private JTextField txtteachunit;
 	private JLabel lblLearnHours;
-	private JTextField textField_3;
+	private JTextField txtlearnhours;
 	private JList list;
+	private JLabel lblPrecourses;
 
 	/**
 	 * Create the panel.
@@ -23,7 +25,7 @@ public class AddCourseUI extends JPanel {
 	public AddCourseUI() {
 		
 		setBackground(SystemColor.activeCaption);
-		setBounds(10, 59, 424, 300);
+		setBounds(10, 59, 302, 301);
 		setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Course id :");
@@ -31,44 +33,44 @@ public class AddCourseUI extends JPanel {
 		lblNewLabel.setBounds(45, 31, 67, 20);
 		add(lblNewLabel);
 		
-		textField = new JTextField();
-		textField.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
-		textField.setBounds(110, 31, 118, 20);
-		add(textField);
-		textField.setColumns(10);
+		txtcourseid = new JTextField();
+		txtcourseid.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
+		txtcourseid.setBounds(110, 31, 118, 20);
+		add(txtcourseid);
+		txtcourseid.setColumns(10);
 		
 		JLabel lblName = new JLabel("Name :");
 		lblName.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
 		lblName.setBounds(66, 63, 46, 18);
 		add(lblName);
 		
-		textField_1 = new JTextField();
-		textField_1.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
-		textField_1.setColumns(10);
-		textField_1.setBounds(110, 62, 118, 20);
-		add(textField_1);
+		txtcoursename = new JTextField();
+		txtcoursename.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
+		txtcoursename.setColumns(10);
+		txtcoursename.setBounds(110, 62, 118, 20);
+		add(txtcoursename);
 		
 		JLabel lblTeachingUnit = new JLabel("Teaching Unit :");
 		lblTeachingUnit.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
 		lblTeachingUnit.setBounds(20, 92, 92, 20);
 		add(lblTeachingUnit);
 		
-		textField_2 = new JTextField();
-		textField_2.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
-		textField_2.setColumns(10);
-		textField_2.setBounds(110, 92, 118, 20);
-		add(textField_2);
+		txtteachunit = new JTextField();
+		txtteachunit.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
+		txtteachunit.setColumns(10);
+		txtteachunit.setBounds(110, 92, 118, 20);
+		add(txtteachunit);
 		
 		lblLearnHours = new JLabel("Learn Hours :");
 		lblLearnHours.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
 		lblLearnHours.setBounds(30, 123, 82, 20);
 		add(lblLearnHours);
 		
-		textField_3 = new JTextField();
-		textField_3.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
-		textField_3.setColumns(10);
-		textField_3.setBounds(110, 123, 118, 20);
-		add(textField_3);
+		txtlearnhours = new JTextField();
+		txtlearnhours.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
+		txtlearnhours.setColumns(10);
+		txtlearnhours.setBounds(110, 123, 118, 20);
+		add(txtlearnhours);
 		
 		list = new JList();
 		list.setModel(new AbstractListModel() {
@@ -80,8 +82,13 @@ public class AddCourseUI extends JPanel {
 				return values[index];
 			}
 		});
-		list.setBounds(93, 170, 136, 60);
+		list.setBounds(110, 154, 136, 60);
 		add(list);
+		
+		lblPrecourses = new JLabel("Pre-Courses :");
+		lblPrecourses.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
+		lblPrecourses.setBounds(30, 154, 82, 20);
+		add(lblPrecourses);
 
 	}
 }
--- SMS/src/sysAdmin/sysAdminHomeUI.java
+++ SMS/src/sysAdmin/sysAdminHomeUI.java
@@ -3,61 +3,86 @@ package sysAdmin;
 import javax.swing.JPanel;
 import java.awt.SystemColor;
 import javax.swing.JPopupMenu;
+import javax.swing.Painter;
+import javax.swing.UIDefaults;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+import javax.swing.tree.TreePath;
 
 import User.HomeUI;
 import chat.Client;
 
 import java.awt.Component;
+import java.awt.EventQueue;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JButton;
+import javax.swing.JComponent;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import java.awt.Graphics2D;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.GridBagLayout;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.border.CompoundBorder;
 
 public class sysAdminHomeUI extends JPanel {
 
 	/**
 	 * Create the panel.
 	 */
+	sysAdminMenuUI mytree = new sysAdminMenuUI();
+	public JPanel innerpanel = new JPanel();
+	public JPanel contentPane;
+	
+	
 	public sysAdminHomeUI() {
-		setBackground(SystemColor.activeCaption);
-
+		setBackground(Color.GRAY);
 		setBounds(10, 59, 424, 300);
 		setLayout(null);
 		
+		JPanel panel = new JPanel();
+		//panel.setBorder(null);
+		panel.setBounds(0, 0, 173, 300);
+		add(panel);
+		panel.add(mytree.makeUI());
+		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
 		
-		JButton btnAddCourse = new JButton("Add Course");
-		btnAddCourse.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
-		btnAddCourse.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				//((HomeUI)Client.clientGUI).remove(getParent());
-				((HomeUI)Client.clientGUI).contentPane.remove(((HomeUI)Client.clientGUI).innerpanel);
-				((HomeUI)Client.clientGUI).innerpanel = new AddCourseUI();
-				((HomeUI)Client.clientGUI).contentPane.add(((HomeUI)Client.clientGUI).innerpanel);
-				((HomeUI)Client.clientGUI).resizeHome();
-			}
-		});
-		btnAddCourse.setBounds(54, 123, 100, 23);
-		add(btnAddCourse);
+		contentPane = new JPanel();
+		contentPane.setBounds(172, 0, 252, 300);
+		contentPane.setBackground(java.awt.Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		add(contentPane);
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
+		
+		//panel.addInputMethodListener(l);
+		
+		mytree.tree.addMouseListener(new MouseAdapter() {
+		      public void mouseClicked(MouseEvent me) {
+		    		    TreePath tp = mytree.tree.getPathForLocation(me.getX(), me.getY());
+		    		    if(tp != null)
+		    		    {
+		    		    	String s = tp.getLastPathComponent().toString();
+		    		    	if(s.equals("Add New Course"))
+		    		    	{
+		    		    		innerpanel = new AddCourseUI();
+		    		    		contentPane.add(innerpanel);
+		    		    		contentPane.revalidate();
+		    		    		contentPane.repaint();
+		    		    		//lblNewLabel.setText(tp.getLastPathComponent().toString());
+		    		    	}
+		    		    }
+		        }
+		      });
 
 	}
-	private static void addPopup(Component component, final JPopupMenu popup) {
-		component.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			public void mouseReleased(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			private void showMenu(MouseEvent e) {
-				popup.show(e.getComponent(), e.getX(), e.getY());
-			}
-		});
-	}
+	
 }
